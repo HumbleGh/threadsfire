150aca37a2c74dbd7b8e6cc7a09b064bc8dfd49f
HumbleGh <isaacoseianane@gmail.com>
2023-12-18 07:35:21 +0000

    login screen started

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,12 +9,43 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Hey Man"),
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              "assets/images/logo.png",
+              width: 60,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: emailController,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(10.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                hintText: "Enter your email",
+                hintStyle: GoogleFonts.manrope(
+                    fontSize: 14, color: Colors.grey.shade400),
+              ),
+            ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
