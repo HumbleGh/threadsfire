feef93d14f0388fcf7bafec27e58b07b6bfecd2a
HumbleGh <isaacoseianane@gmail.com>
2023-12-18 10:02:50 +0000

    minor changes

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,28 +17,29 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Center(
               child: Image.asset(
                 "assets/images/logo.png",
                 width: 60,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextFormField(
+            const Gap(20),
+            TextFormField(
               controller: emailController,
               cursorColor: Colors.black,
               decoration: InputDecoration(
                 contentPadding: const EdgeInsets.all(10.0),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.grey.shade400),
+                  borderSide: const BorderSide(color: Colors.black),
                 ),
                 focusedBorder: const OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black),
@@ -47,10 +49,8 @@ class _SplashScreenState extends State<SplashScreen> {
                     fontSize: 14, color: Colors.grey.shade400),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextFormField(
+            const Gap(20),
+            TextFormField(
               controller: passwordlController,
               obscureText: true,
               cursorColor: Colors.black,
@@ -58,7 +58,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 contentPadding: const EdgeInsets.all(10.0),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.grey.shade400),
+                  borderSide: const BorderSide(color: Colors.black),
                 ),
                 focusedBorder: const OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black),
@@ -68,8 +68,49 @@ class _SplashScreenState extends State<SplashScreen> {
                     fontSize: 14, color: Colors.grey.shade400),
               ),
             ),
-          ),
-        ],
+            const Gap(10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                    onTap: () {}, child: const Text("Forgot Passsword"))
+              ],
+            ),
+            const Gap(30),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(8)),
+              child: Text(
+                "Login",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.manrope(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: GoogleFonts.manrope(fontSize: 14, color: Colors.black),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Create Account",
+                      style: GoogleFonts.manrope(
+                          fontWeight: FontWeight.bold, color: Colors.black),
+                    ))
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
