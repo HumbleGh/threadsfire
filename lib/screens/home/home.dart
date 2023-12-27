585090fbd2ff9074ee6706b40f4b0e33d576b308
HumbleGh <isaacoseianane@gmail.com>
2023-12-27 06:28:23 +0000

    started firebase integration

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -16,82 +16,109 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
             child: Column(
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      backgroundImage: AssetImage("assets/images/user.jpeg"),
-                    ),
-                    const Gap(8),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "Humble",
-                                style: GoogleFonts.manrope(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Spacer(),
-                              Text(
-                                "5mins",
-                                style: GoogleFonts.manrope(fontSize: 10),
-                              ),
-                              const Gap(6),
-                              const Icon(Icons.more_horiz),
-                            ],
-                          ),
-                          Text(
-                            "Hey man do well to call me as soon as possible"
-                            " so we can discuss your future",
-                            style: GoogleFonts.manrope(fontSize: 14),
-                          ),
-                          const Gap(10),
-                          Row(
-                            children: [
-                              SvgPicture.asset(
-                                "assets/icons/heart.svg",
-                                width: 18,
-                                color: Colors.black,
-                              ),
-                              const Gap(10),
-                              SvgPicture.asset(
-                                "assets/icons/comment.svg",
-                                width: 16,
-                              ),
-                              const Gap(10),
-                              SvgPicture.asset(
-                                "assets/icons/repeat.svg",
-                                width: 18,
-                              ),
-                              const Gap(10),
-                              SvgPicture.asset(
-                                "assets/icons/send.svg",
-                                width: 16,
-                              ),
-                              const Gap(10),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                ThreadsMessage(),
+                ThreadsMessage(),
+                ThreadsMessage(),
+                ThreadsMessage(),
+                ThreadsMessage(),
+                ThreadsMessage(),
+                ThreadsMessage(),
+                ThreadsMessage(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class ThreadsMessage extends StatelessWidget {
+  const ThreadsMessage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage("assets/images/user.jpeg"),
+            ),
+            const Gap(8),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Humble",
+                        style: GoogleFonts.manrope(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        "5min",
+                        style: GoogleFonts.manrope(fontSize: 10),
+                      ),
+                      const Gap(6),
+                      const Icon(Icons.more_horiz),
+                    ],
+                  ),
+                  Text(
+                    "Hey man do well to call me as soon as possible"
+                    " so we can discuss your future",
+                    style: GoogleFonts.manrope(fontSize: 14),
+                  ),
+                  const Gap(10),
+                  Row(
+                    children: [
+                      SvgPicture.asset(
+                        "assets/icons/heart.svg",
+                        width: 18,
+                        color: Colors.black,
+                      ),
+                      const Gap(10),
+                      SvgPicture.asset(
+                        "assets/icons/comment.svg",
+                        width: 16,
+                      ),
+                      const Gap(10),
+                      SvgPicture.asset(
+                        "assets/icons/repeat.svg",
+                        width: 18,
+                      ),
+                      const Gap(10),
+                      SvgPicture.asset(
+                        "assets/icons/send.svg",
+                        width: 16,
+                      ),
+                      const Gap(10),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const Gap(10),
+        Divider(
+          color: Colors.grey.shade300,
+        ),
+        const Gap(10)
+      ],
     );
   }
 }
