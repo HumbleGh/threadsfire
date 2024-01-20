464b1eaeb4beb5b05d595fb656a1a7082a4f3697
HumbleGh <isaacoseianane@gmail.com>
2024-01-20 23:51:51 +0000

    making progress

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                 ),
-                Gap(20),
+                const Gap(20),
                 ListView.builder(
                     itemCount: threadMessages.length,
                     itemBuilder: (context, index) {
