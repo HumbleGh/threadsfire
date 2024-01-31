f43288bbef18575ff89484b2ce56355eacfaf0d6
HumbleGh <isaacoseianane@gmail.com>
2024-01-31 23:27:35 +0000

    minor changes

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -12,7 +12,7 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Search Page Yeah"),
+        child: Text("My Search Page"),
       ),
     );
   }
