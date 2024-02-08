ce3cd753bc3796655f385ff376d5683e6f925cfa
HumbleGh <isaacoseianane@gmail.com>
2024-02-08 00:59:34 +0000

    minor changes

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -14,39 +14,52 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: SingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Search",
-              style: GoogleFonts.manrope(
-                  fontSize: 24, fontWeight: FontWeight.w700),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
             ),
-            const Gap(10),
-            Container(
-              width: double.infinity,
-              height: 44,
-              decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: Colors.grey),
-                  borderRadius: BorderRadius.circular(12)),
-              child: TextFormField(
-                cursorColor: Colors.black,
-                decoration: const InputDecoration(
-                  hintText: "Search",
-                  prefixIcon: Icon(Icons.search),
-                  border: InputBorder.none,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Search",
+                  style: GoogleFonts.manrope(
+                      fontSize: 24, fontWeight: FontWeight.w700),
                 ),
-              ),
-            )
-          ],
+                const Gap(10),
+                Container(
+                  width: double.infinity,
+                  height: 44,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: const InputDecoration(
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                const SuggestedFollower(),
+              ],
+            ),
+          ),
         ),
       ),
-    )));
+    );
+  }
+}
+
+// Class for suggested profiles to follow
+class SuggestedFollower extends StatelessWidget {
+  const SuggestedFollower({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile();
   }
 }
